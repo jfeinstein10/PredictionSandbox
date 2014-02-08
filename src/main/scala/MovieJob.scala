339948418b88307cf0ae4c0c4fb207e0357f0617
Jeremy Feinstein <jfeinstein10@gmail.com>
2014-02-08 15:55:17 -0500

    Cleaning up and uncommenting MovieJob

--- src/main/scala/MovieJob.scala
+++ src/main/scala/MovieJob.scala
@@ -7,18 +7,11 @@ import scala.math.pow
  * Created by jeremy on 2/8/14.
  */
 class MovieJob(args: Args) extends Job(args) {
-  TextLine( args("input") )
-    .flatMap('line -> 'word) { line : String => tokenize(line) }
-    .groupBy('word) { _.size }
-    .write( Tsv( args("output") ) )
 
-  // Split a piece of text into individual words.
-  def tokenize(text : String) : Array[String] = {
-    // Lowercase each word and remove punctuation.
-    text.toLowerCase.replaceAll("[^a-zA-Z0-9\\s]", "").split("\\s+")
-  }
+  val input = TextLine(args("input"))
+  val output = TextLine(args("output"))
 
-  /*val n = 10 // number of points
+  val n = 10 // number of points
   val d = 2 // degree of the polynomial
   val arr = DenseVector.zeros[Double](n)
   for (i <- 0 to n-1) arr(i) = i
@@ -26,18 +19,10 @@ class MovieJob(args: Args) extends Job(args) {
   /**
    * Input, x values (time)
    */
-  val X = DenseVector.horzcat(DenseVector.ones[Double](n),  arr)
-
-  /**
-   * Input, y values (number of shares, likes, etc.)
-   */
-  var t =  arr.map(pow(_,2))
-
-  println(t)
-
-  println(X)
-
-
+  var X = DenseVector.horzcat(DenseVector.ones[Double](n),  arr)
+  for (i <- 2 to d) {
+    X = DenseMatrix.horzcat(X, arr.map(pow(_,i)).toDenseMatrix.t)
+  }
 
   /**
    * Lambda is a parameter. The higher it is, less likely the matrix will overfit data (smoother curve).
@@ -48,21 +33,11 @@ class MovieJob(args: Args) extends Job(args) {
 
   println(lambdaMatrix)
 
-  /**
-   * Regression
-   */
-  //val w2 = (lambdaMatrix + X.t.dot(X)) / X.t.dot(t)
-  /**
-   * Weight vector
-   */
-  val w2 = LinearRegression.regress(arr.toDenseMatrix, t)
-  println(w2)
-  /*val csv = TextLine("movies.csv")
+  val csv = input
     .map('line -> 'slope) {
     line: String =>
-      val target = DenseVector[Double](line.split(",").map(_.toDouble))
-      val result = LinearRegression.regress(matrix, target)
-      println(result)
-      result(0)
-  }*/*/
+      val target = DenseVector[Double](line.split(",").slice(0, n).map(_.toDouble))
+      val result = LinearRegression.regress(X, target)
+      result(1)
+  }.write(output)
 }
